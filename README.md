d0ae31d0707316ea51f009a4c10c8f985fd38c37
Patrik Svensson <patrik@patriksvensson.se>
2023-12-09 16:04:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# World Flags
+
+This is a small project I created to learn a bit about iOS development using Swift and SwiftUI.
---
+++ world-flags/world_flagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  world_flagsApp.swift
+//  world-flags
+//
+//  Created by Patrik Svensson on 2023-12-08.
+//
+
+import SwiftUI
+
+@main
+struct world_flagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
